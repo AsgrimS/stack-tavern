661557b58df495628f3051622df1b484874204e9
AsgrimS <jan.semik@gmail.com>
2023-06-22 18:00:34 +0200

    show data from api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+	import { CodeBlock } from '@skeletonlabs/skeleton';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<CodeBlock language="JSON" code={JSON.stringify(data, null, 2)} />
