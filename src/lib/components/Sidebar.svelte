3a44bfb0bd6b83387144525d09a800aaab99cbb2
AsgrimS <jan.semik@gmail.com>
2023-06-22 16:57:37 +0200

    base setup

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { AppRail, AppRailAnchor, AppRailTile } from '@skeletonlabs/skeleton';
+	import { IconBrandGithub } from '@tabler/icons-svelte';
+
+	let currentTile = 0;
+</script>
+
+<AppRail>
+	<svelte:fragment slot="lead">
+		<AppRailAnchor href="/">(icon)</AppRailAnchor>
+	</svelte:fragment>
+	<!-- --- -->
+	<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		<span>Tile 1</span>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		<span>Tile 2</span>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		<span>Tile 3</span>
+	</AppRailTile>
+	<!-- --- -->
+	<svelte:fragment slot="trail">
+		<AppRailAnchor
+			href="https://github.com/AsgrimS/stack-tavern"
+			target="_blank"
+			title="Repository"
+		>
+			<IconBrandGithub size={48} class="mx-auto" /></AppRailAnchor
+		>
+	</svelte:fragment>
+</AppRail>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	import '../app.postcss';
+
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+
+	import Sidebar from '$lib/components/Sidebar.svelte';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>Stack Tavern</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<Sidebar />
+	</svelte:fragment>
+	<slot />
+</AppShell>
