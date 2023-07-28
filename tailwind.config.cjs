a8a314060555d0eb4b631b1cd8de54166fbca045
AsgrimS <jan.semik@gmail.com>
2023-07-28 21:11:04 +0200

    format

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,7 @@ module.exports = {
 	content: [
 		"./src/**/*.{html,js,svelte,ts}",
 		// eslint-disable-next-line @typescript-eslint/no-var-requires
-		require("path").join(
-			require.resolve("@skeletonlabs/skeleton"),
-			"../**/*.{html,js,svelte,ts}"
-		),
+		require("path").join(require.resolve("@skeletonlabs/skeleton"), "../**/*.{html,js,svelte,ts}"),
 	],
 	theme: {
 		extend: {},
